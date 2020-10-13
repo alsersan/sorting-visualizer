4575c4bd8eb1d50d1edbad25e506b9f9d762734d
Alvaro Serrano <alsersan20@gmail.com>
2020-10-13 23:21:30 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import BarChart from "./components/BarChart";
 
-function App() {
+const styles = {
+  container: {
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <BarChart />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/BarChart.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const styles = {
+  mainContainer: {
+    height: "700px",
+    width: "1200px",
+
+    backgroundColor: "#ccc",
+  },
+};
+
+const BarChart = () => {
+  return <div style={styles.mainContainer} />;
+};
+
+export default BarChart;
