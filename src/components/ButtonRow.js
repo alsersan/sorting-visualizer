0009fbdc55187a5071db875ef7db7f7593f8d245
Alvaro Serrano <alsersan20@gmail.com>
2020-10-27 23:37:54 +0100

    Added icons to ButtonRow

--- src/components/ButtonRow.js
+++ src/components/ButtonRow.js
@@ -1,31 +1,57 @@
 import React from "react";
+import styled from "styled-components";
+import { FaPlay } from "react-icons/fa";
+import { FaPause } from "react-icons/fa";
+import { FaStepForward } from "react-icons/fa";
+import { FaStepBackward } from "react-icons/fa";
 
 import {
   recursiveBubbleSort,
   stop,
 } from "../algorithms/recursiveSortingAlgorithms";
 
+const StyledButtonRow = styled.div`
+  margin-top: 1.5rem;
+  display: flex;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  padding: 1rem;
+  border-radius: 50px;
+  cursor: pointer;
+  border: none;
+  outline: none;
+  margin: 0 0.5rem;
+`;
+
 const ButtonRow = ({ speed, array, arr, setStart, setIsSorted }) => {
   return (
-    <React.Fragment>
-      <button
+    <StyledButtonRow>
+      <Button>
+        <FaStepBackward size={30} />
+      </Button>
+      <Button
         onClick={() => {
           recursiveBubbleSort(speed, arr, setIsSorted);
           setStart(true);
         }}
       >
-        Start
-      </button>
+        <FaPlay size={30} />
+      </Button>
       {console.log(speed)}
-      <button
+      <Button
         onClick={() => {
           stop();
           setStart(false);
         }}
       >
-        Stop
-      </button>
-    </React.Fragment>
+        <FaPause size={30} />
+      </Button>
+      <Button>
+        <FaStepForward size={30} />
+      </Button>
+    </StyledButtonRow>
   );
 };
 
