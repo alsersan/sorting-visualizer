9a0c6ce63607e9d7e56f63c61e1abc819652b89c
Alvaro Serrano <alsersan20@gmail.com>
2020-11-15 23:41:47 +0100

    Slider: changed styles

--- src/components/Sidebar/components/Slider.js
+++ src/components/Sidebar/components/Slider.js
@@ -4,6 +4,44 @@ import styled from "styled-components";
 const StyledSlider = styled.input`
   width: 100%;
   cursor: pointer;
+  appearance: none;
+  outline: none;
+  height: 2px;
+  background-color: #555;
+
+  &::-webkit-slider-thumb {
+    appearance: none;
+    height: 26px;
+    width: 26px;
+    border-radius: 50%;
+    background-color: #f6f6f6;
+    border: 2px solid #555;
+
+    &:hover {
+      border: 2px solid #1e88e5;
+    }
+
+    &:active {
+      background-color: #1e88e5;
+    }
+  }
+
+  &::-moz-range-thumb {
+    appearance: none;
+    height: 22px;
+    width: 22px;
+    border-radius: 50%;
+    background-color: #f6f6f6;
+    border: 2px solid #555;
+
+    &:hover {
+      border: 2px solid #1e88e5;
+    }
+
+    &:active {
+      background-color: #1e88e5;
+    }
+  }
 `;
 
 const Slider = (props) => {
