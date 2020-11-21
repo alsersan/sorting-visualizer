9e6a1b5c931e48f1e23c8af51f0602fe5657036a
Alvaro Serrano <alsersan20@gmail.com>
2020-11-21 19:21:17 +0100

    remove Tabs and VisualMode components

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
 import PlaygroundMode from "./components/PlaygroundMode";
-import VisualMode from "./components/VisualMode";
-
-import Tabs from "./components/Tabs";
 
 const StyledSidebar = styled.div`
   height: 100%;
@@ -17,19 +14,9 @@ const StyledSidebar = styled.div`
 `;
 
 const Sidebar = () => {
-  const [activeTab, setActiveTab] = useState(0);
-
   return (
     <StyledSidebar>
-      {console.log(activeTab)}
-      <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
-      {activeTab === 0 ? (
-        <PlaygroundMode />
-      ) : activeTab === 1 ? (
-        <VisualMode />
-      ) : (
-        <span>Tab 3</span>
-      )}
+      <PlaygroundMode />
     </StyledSidebar>
   );
 };
