ebdf8866a7b222cc8c5124896cfff6958dbc4a7f
Alvaro Serrano <alsersan20@gmail.com>
2020-11-21 21:09:40 +0100

    move global style to a separate component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-import { createGlobalStyle } from "styled-components";
+import { GlobalStyle } from "./components/GlobalStyle";
 
 import Sidebar from "./components/Sidebar";
 import BarChartContainer from "./components/BarChartContainer";
-import StyledThemeProvider from "./StyledThemeProvider";
+import StyledThemeProvider from "./components/StyledThemeProvider";
 
 import { SortingOptionsProvider } from "./contexts/SortingOptionsContext";
 import { AlgorithmProvider } from "./contexts/AlgorithmContext";
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-
-  body {
-  font-family: ${(props) => props.theme.main.fontFamily};
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  }
-`;
 
 const MainContainer = styled.div`
   height: 100vh;
---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+font-family: ${(props) => props.theme.main.fontFamily};
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+}
+`;
