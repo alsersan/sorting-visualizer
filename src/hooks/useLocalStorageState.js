200bbc79cf35ce7c4e463f65ee4ac074a52bba38
Alvaro Serrano <alsersan20@gmail.com>
2021-01-21 23:22:28 +0100

    fix performance issue with custom hook

--- src/hooks/useLocalStorageState.js
+++ src/hooks/useLocalStorageState.js
@@ -8,36 +8,14 @@ const useLocalStorageState = (key, defaultValue = "") => {
   });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state));
+    let timeout;
+    timeout = setTimeout(() => {
+      localStorage.setItem(key, JSON.stringify(state));
+    }, 150);
+    return () => clearTimeout(timeout);
   }, [key, state]);
 
   return [state, setState];
 };
 
-// function useLocalStorageState(
-//   key,
-//   defaultValue = '',
-//   {serialize = JSON.stringify, deserialize = JSON.parse} = {},
-// ) {
-//   const [state, setState] = React.useState(() => {
-//     const valueInLocalStorage = window.localStorage.getItem(key)
-//     if (valueInLocalStorage) {
-//       return deserialize(valueInLocalStorage)
-//     }
-//     return typeof defaultValue === 'function' ? defaultValue() : defaultValue
-//   })
-
-//   const prevKeyRef = React.useRef(key)
-
-//   React.useEffect(() => {
-//     const prevKey = prevKeyRef.current
-//     if (prevKey !== key) {
-//       window.localStorage.removeItem(prevKey)
-//     }
-//     prevKeyRef.current = key
-//     window.localStorage.setItem(key, serialize(state))
-//   }, [key, state, serialize])
-
-//   return [state, setState]
-// }
 export default useLocalStorageState;
