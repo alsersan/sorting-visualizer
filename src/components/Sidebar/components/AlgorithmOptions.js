b6286e1e99ca3d8df1c2d4178408d148b8116922
Alvaro Serrano <alsersan20@gmail.com>
2021-01-23 00:07:05 +0100

    algorithmOptions: use styles from Button component

--- src/components/Sidebar/components/AlgorithmOptions.js
+++ src/components/Sidebar/components/AlgorithmOptions.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
+import Button from "./Button";
 import ComponentContainer from "./ComponentContainer";
 import { useAlgorithmContext } from "../../../contexts/AlgorithmContext";
 
@@ -14,24 +15,12 @@ const Radio = styled.input`
   display: none;
 `;
 
-const StyledLabel = styled.label`
-  display: flex;
-  flex-grow: 1;
-  align-items: center;
-  justify-content: center;
-  border: none;
-  border-radius: 3rem;
-  outline: none;
-  font-size: 1rem;
-  font-family: inherit;
-  color: inherit;
-  box-shadow: ${({ theme }) => theme.button.boxShadow};
+const StyledLabel = styled(Button)`  
+  flex-grow: 1;  
   height: 2rem;
   width: 8rem;
   max-width: 8rem;
-  margin: 0.5rem;
-  background-color: ${({ theme }) => theme.backgroundColor.secondary};
-  cursor: pointer;
+  margin: 0.5rem; 
 
   ${Radio}:checked + & {
     color: ${({ theme }) => theme.button.activeText};
@@ -63,6 +52,7 @@ const AlgorithmOptions = () => {
               onChange={() => setActiveOption(index)}
             />
             <StyledLabel
+              forwardedAs="label"
               htmlFor={`${option} button`.toLowerCase().replace(/ /g, "-")}
             >
               {option}
