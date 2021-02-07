4a6799d2a165fd35119b8ab6a353fe635a1f1845
Alvaro Serrano <alsersan20@gmail.com>
2021-02-07 19:37:57 +0100

    save fonts locally

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,6 +1,28 @@
 import { createGlobalStyle } from 'styled-components';
 
+import UbuntuWoff from '../fonts/Ubuntu.woff';
+import UbuntuWoff2 from '../fonts/Ubuntu.woff2';
+import UbuntuMediumWoff from '../fonts/UbuntuMedium.woff';
+import UbuntuMediumWoff2 from '../fonts/UbuntuMedium.woff2';
+
 export const GlobalStyle = createGlobalStyle`
+
+@font-face {
+  font-family: 'Ubuntu';
+  src: url(${UbuntuWoff2}) format('woff2'),
+  url(${UbuntuWoff}) format('woff');   
+  font-weight: normal;
+  font-style: normal   
+}
+
+@font-face {
+  font-family: 'Ubuntu';
+  src: url(${UbuntuMediumWoff2}) format('woff2'),
+  url(${UbuntuMediumWoff}) format('woff');   
+  font-weight: 500;
+  font-style: normal   
+}
+
 * {
   box-sizing: border-box;
   margin: 0;
@@ -8,7 +30,7 @@ export const GlobalStyle = createGlobalStyle`
 }
 
 body {
-font-family: ${(props) => props.theme.fontFamily};
+font-family: 'Ubuntu', sans-serif;
 color: ${(props) => props.theme.textColor}
 }
 `;
